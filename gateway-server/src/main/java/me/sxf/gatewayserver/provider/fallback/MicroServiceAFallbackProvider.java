403a3e6dcf1bec167e81798e8af47615ad7e3812
paul <784413317@qq.com>
2018-12-05 13:55:25 +0800

    config

---
+++ gateway-server/src/main/java/me/sxf/gatewayserver/provider/fallback/MicroServiceAFallbackProvider.java
@@ -0,0 +1,67 @@
+package me.sxf.gatewayserver.provider.fallback;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * MicroServiceA 调用失败回调，降级服务
+ */
+@Component
+public class MicroServiceAFallbackProvider implements FallbackProvider {
+
+
+    @Override
+    public ClientHttpResponse fallbackResponse(Throwable cause) {
+        return fallbackResponse();
+    }
+
+    @Override
+    public String getRoute() {
+        return "microservicea";
+    }
+
+    @Override
+    public ClientHttpResponse fallbackResponse() {
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return this.getStatusCode().value();
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return this.getStatusCode().getReasonPhrase();
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("{\"msg\":\"MicroServiceA is unable\"}".getBytes());
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
